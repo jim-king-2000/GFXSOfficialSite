4ba0dda22eb5ea2d54988d7adef147348361652b
Jim Jin <jim_king_2000@163.com>
2021-09-16 16:41:29 +0800

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import MyHeader from '../components/MyHeader'
 import MyBody from '../components/MyBody'
 import MyFooter from '../components/MyFooter'
-import { Grommet } from 'grommet'
+import { Grommet, Box } from 'grommet'
 import { grommet } from 'grommet/themes'
 
 const WIDTH = '1440px'
 
 function HomePage() {
   return <Grommet full theme={grommet}>
-    <MyHeader width={WIDTH} />
-    <MyBody width={WIDTH} />
-    <MyFooter width={WIDTH} />
+    <Box flex={false} responsive={false} width={{ min: WIDTH }} overflow='auto'>
+      <MyHeader width={WIDTH} />
+      <MyBody width={WIDTH} />
+      <MyFooter width={WIDTH} />
+    </Box>
   </Grommet>
 }
 
