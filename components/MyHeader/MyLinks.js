4089f01d486f74bbc50f3c938d8ec1552d8a727a
Jim Jin <jim_king_2000@163.com>
2021-09-16 16:22:29 +0800

    trivial change

--- components/MyHeader/MyLinks.js
+++ components/MyHeader/MyLinks.js
@@ -4,21 +4,21 @@ function ProductCenter() {
   return <DropButton
     dropAlign={{ top: 'bottom', left: 'left' }}
     dropContent={
-      <Box direction='row' gap='small' margin='large'>
+      <Box direction='row' gap='large' margin='large'>
         <Box gap='small'>
-          <Heading level={3}>视觉类</Heading>
+          <Heading level={3}>模组</Heading>
           <Anchor>产品1</Anchor>
           <Anchor>产品2</Anchor>
           <Anchor>产品3</Anchor>
         </Box>
         <Box gap='small'>
-          <Heading level={3}>位置类</Heading>
+          <Heading level={3}>核心板</Heading>
           <Anchor>产品4</Anchor>
           <Anchor>产品5</Anchor>
           <Anchor>产品6</Anchor>
         </Box>
         <Box gap='small'>
-          <Heading level={3}>工具类</Heading>
+          <Heading level={3}>开发板</Heading>
           <Anchor>产品7</Anchor>
           <Anchor>产品8</Anchor>
           <Anchor>产品9</Anchor>
