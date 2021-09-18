c8c3c35293346168f4a8f5a56828a791f4405672
Jim Jin <jim_king_2000@163.com>
2021-09-18 13:15:13 +0800

    add title

--- components/withLayout.js
+++ components/withLayout.js
@@ -1,21 +1,29 @@
 import MyHeader from '../components/MyHeader'
 import MyBody from '../components/MyBody'
 import MyFooter from '../components/MyFooter'
+import Head from 'next/head'
 import { Grommet, Box } from 'grommet'
 import { grommet } from 'grommet/themes'
+import useTranslation from 'next-translate/useTranslation'
 
 const WIDTH = '1440px'
 
 function withLayout(Content) {
-  return () => (
-    <Grommet full theme={grommet}>
-      <Box flex={false} responsive={false} width={{ min: WIDTH }} overflow='auto'>
-        <MyHeader width={WIDTH} />
-        <MyBody width={WIDTH} Content={Content} />
-        <MyFooter width={WIDTH} />
-      </Box>
-    </Grommet>
-  )
+  return () => {
+    const { t } = useTranslation('common')
+    return (
+      <Grommet full theme={grommet}>
+        <Head>
+          <title>{t('brand')}</title>
+        </Head>
+        <Box flex={false} responsive={false} width={{ min: WIDTH }} overflow='auto'>
+          <MyHeader width={WIDTH} />
+          <MyBody width={WIDTH} Content={Content} />
+          <MyFooter width={WIDTH} />
+        </Box>
+      </Grommet>
+    )
+  }
 }
 
 export default withLayout
