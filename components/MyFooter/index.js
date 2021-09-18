bcb30e9b69357ad65f59cd99bcc6e0a799c44238
Jim Jin <jim_king_2000@163.com>
2021-09-18 14:14:38 +0800

    add copy right

--- components/MyFooter/index.js
+++ components/MyFooter/index.js
@@ -1,12 +1,15 @@
-import { Box } from 'grommet'
 import LngSelector from './LngSelector'
+import { Box, Text } from 'grommet'
+import useTranslation from 'next-translate/useTranslation'
 
 function MyFooter({ width }) {
+  const { t } = useTranslation('common')
   return (
     <Box background='light-1' align='center' pad='medium'>
       <Box width={width} direction='row'>
         <LngSelector />
       </Box>
+      <Text>{t('copy-right')}</Text>
     </Box>
   )
 }
