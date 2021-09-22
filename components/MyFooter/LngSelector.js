c1c3bf326ce7783e5e0fb0377d0cc98e1f29c33f
Jim Jin <jim_king_2000@163.com>
2021-09-22 11:36:32 +0800

    add country flags to the lng selector

--- components/MyFooter/LngSelector.js
+++ components/MyFooter/LngSelector.js
@@ -2,12 +2,13 @@ import i18nConfig from '../../i18n.json'
 import { Select, Text, Box } from 'grommet'
 import useTranslation from 'next-translate/useTranslation'
 import setLanguage from 'next-translate/setLanguage'
+import Flag from 'react-flagkit'
 
 const { locales } = i18nConfig
 
 const lngMap = new Map([
-  ['zh', <Text>中文</Text>],
-  ['en', <Text>English</Text>]
+  ['zh', <Box direction='row' gap='small' margin='small'><Flag country='CN' /><Text>中文</Text></Box>],
+  ['en', <Box direction='row' gap='small' margin='small'><Flag country='US' /><Text>English</Text></Box>]
 ])
 
 function LngSelector() {
