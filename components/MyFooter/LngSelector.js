998c728fca4612f9ee2f5405b5b77ca623c5473f
Jim Jin <jim_king_2000@163.com>
2021-09-22 10:47:34 +0800

    fix typo

--- components/MyFooter/LngSelector.js
+++ components/MyFooter/LngSelector.js
@@ -2,7 +2,6 @@ import i18nConfig from '../../i18n.json'
 import { Select, Text, Box } from 'grommet'
 import useTranslation from 'next-translate/useTranslation'
 import setLanguage from 'next-translate/setLanguage'
-import Flags from 'country-flag-icons/react/3x2'
 
 const { locales } = i18nConfig
 
--- components/MyHeader/MyLogo.js
+++ components/MyHeader/MyLogo.js
@@ -5,7 +5,7 @@ function MyLogo({ size }) {
   return (
     <Box direction='row' gap='small' align='center' margin='small'>
       <Webcam size={size} />
-      <Text size={size}>光方<br />视讯</Text>
+      <Text size={size}>光方<br />讯视</Text>
     </Box>
   )
 }
