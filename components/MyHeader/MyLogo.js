c0af724d04bf244a72806353af435cf59967de46
Jim Jin <jim_king_2000@163.com>
2021-09-22 13:40:50 +0800

    fix the typo of logo

--- components/MyHeader/MyLogo.js
+++ components/MyHeader/MyLogo.js
@@ -1,11 +1,13 @@
 import { Box, Text } from 'grommet'
 import { Webcam } from 'grommet-icons'
+import useTranslation from 'next-translate/useTranslation'
 
 function MyLogo({ size }) {
+  const { t } = useTranslation('common')
   return (
     <Box direction='row' gap='small' align='center' margin='small'>
       <Webcam size={size} />
-      <Text size={size}>光方<br />讯视</Text>
+      <Text size={size}>{t('brand')}</Text>
     </Box>
   )
 }
